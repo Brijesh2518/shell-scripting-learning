9009818aee6fc3be1ea444f4076824c9437845a4
Brijesh2518 <brijeshsharma2518@gmail.com>
2026-03-20 15:35:20 +0530

    New Script

---
+++ first_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is my first shell script
+
+echo "Hello, World!"
+echo "Welcome to Shell Scripting"
+
+# Taking input from user
+echo "Enter your name:"
+read name
+
+echo "Hello $name, nice to meet you!"
